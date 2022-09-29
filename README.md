dbc8ca59c134aeddd6a2f9b50b26fd57cbfd3ff5
Hamir Mahal <hamirmahal@gmail.com>
2022-09-28 20:20:54 -0700

    docs: add Java hashmap example

---
+++ README.md
@@ -0,0 +1,5 @@
+# Instructions
+You might be able to use a command like this to run a program in this repository.
+```sh
+java programName.java
+```
---
+++ hashmap.java
@@ -0,0 +1,39 @@
+import java.util.HashMap; // import the HashMap class
+
+class Main {
+  public static void main(String args[]) {
+    System.out.println("Hello, world!");
+    HashMap<String, String> a = new HashMap<>();
+    a.put("abc", "a");
+    a.put("def", "a");
+    a.put("172.27.17.7:3001", "a");
+    boolean result0 = a.containsKey("abc");
+    boolean result1 = a.containsKey("def");
+    boolean result2 = a.containsKey("172.27.17.7:3001");
+    System.out.println(result0);
+    System.out.println(result1);
+    System.out.println(result2);
+  }
+}
+
+/*
+https://replit.com/languages/java10
+
+import java.util.HashMap; // import the HashMap class
+
+class Main {
+  public static void main(String args[]) {
+    System.out.println("Hello, world!");
+    HashMap<String, String> a = new HashMap<>();
+    a.put("abc", "a");
+    a.put("def", "a");
+    a.put("172.27.17.7:3001", "a");
+    boolean result0 = a.containsKey("abc");
+    boolean result1 = a.containsKey("def");
+    boolean result2 = a.containsKey("172.27.17.7:3001");
+    System.out.println(result0);
+    System.out.println(result1);
+    System.out.println(result2);
+  }
+}
+ */
